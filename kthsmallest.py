e18a15e487cf4554b3db3029b2e17160b6bee9cd
warren <wmowkm@gmail.com>
2020-03-22 00:17:47 -0400

    added recursive solution - O(n)

--- kthsmallest.py
+++ kthsmallest.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import heapq
+from random import randint
 
 def kthSmallestBrute(arr, k):
     arrSorted = sorted(arr)
@@ -16,9 +17,43 @@ def kthSmallestHeap(arr, k):
             continue
     return maxHeap[0] * -1
 
+def kthSmallestRecursive(arr, k):
+    n = len(arr)
+    pivotInd = randint(0, n-1)
+    pivot = arr[pivotInd]
+
+    smaller = []
+    larger = []
+    for i, a in enumerate(arr):
+        if i == pivotInd:
+            continue
+        elif a < pivot:
+            smaller.append(a)
+        else:
+            larger.append(a)
+
+    #print('looking for the {}-th smallest'.format(k))
+    #print('pivot:\t\t{}'.format(pivot))
+    #print('smaller:\t{}'.format(smaller))
+    #print('larger:\t\t{}'.format(larger))
+
+    nS = len(smaller)
+    if nS == k - 1:
+        return pivot
+    elif nS > k - 1:
+        return kthSmallestRecursive(smaller, k)
+    else:
+        return kthSmallestRecursive(larger, k-nS-1)
+
 
 if __name__ == "__main__":
-    tests = [[17, 42, 5, 0, 10, -3, 2, 9]] 
+    tests = [[17, 42, 5, 0, 10, -3, 2, 9]]
+    k = 3
     for test in tests:
-        print(kthSmallestBrute(test, 3))
-        print(kthSmallestHeap(test, 3))
+        print(test)
+        for f in [kthSmallestBrute, kthSmallestHeap, kthSmallestRecursive]:
+            #print(test)
+            #print(kthSmallestBrute(test, k))
+            #print(kthSmallestHeap(test, k))
+            #print(kthSmallestRecursive(test, k))
+            print(f(test, k))
