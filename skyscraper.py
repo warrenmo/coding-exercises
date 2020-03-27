16f0cd3614ab44aef7b7322fc3d0f2f46ae7024c
warren <wmowkm@gmail.com>
2020-03-27 16:58:10 -0400

    removed redundant code, added easier testing of smarter strategies

--- skyscraper.py
+++ skyscraper.py
@@ -6,14 +6,23 @@ from collections import defaultdict
 
 
 class Skyscraper(object):
-    def __init__(self, clues, obvious=True):
+    def __init__(self, clues, use_obvious=True, use_smart_cell_order=True,
+                 use_update_vals=True):
         self.clues = clues
-        self.obvious = obvious
+        self.use_obvious = use_obvious
+        self.use_smart_cell_order = use_smart_cell_order
+        self.use_update_vals = use_update_vals
 
-        self.n = len(clues) // 4
-        self.board = self.starting_board()
         self.num_guesses = 0
-        #print(self.board)
+        self.n = len(clues) // 4
+
+        self.pos_vals, self.num_vals = self.get_values()
+        self.board = self.starting_board()
+        if not self.use_smart_cell_order:
+            self.cell_order = self.naive_cell_order()
+        else:
+            # TODO: repeats obvious calculations
+            self.cell_order = self.smart_cell_order()
 
     @staticmethod
     def column(matrix, j):
@@ -72,10 +81,6 @@ class Skyscraper(object):
         clue_lt_ = exists_clue(clue_lt, seen_lt)
         clue_rb_ = exists_clue(clue_rb, seen_rb)
         return clue_lt_ == seen_lt and clue_rb_ == seen_rb
-        #if clue_lt_ == seen_lt and clue_rb_ == seen_rb:
-        #    return True
-        #else:
-        #    return False
 
     def possible_lines(self, clue_lt, clue_rb):
         # find what lines are possible given two clues
@@ -99,13 +104,12 @@ class Skyscraper(object):
             pos_vals[index] = vals
         return pos_vals, num_lines
 
-    def possible_values(self):
+    def get_values(self):
         row_clues = []
         col_clues = []
         row_vals = {}
         col_vals = {}
         num_vals = {}
-        #col_num_vals = {}
         for i in range(self.n):
             rv, r_num = self.line_values(i, is_row=True)
             cv, c_num = self.line_values(i, is_row=False)
@@ -113,7 +117,6 @@ class Skyscraper(object):
             col_vals.update(cv)
             num_vals['row-' + str(i)] = r_num
             num_vals['col-' + str(i)] = c_num
-            #col_num_vals[i] = c_num
         #all_vals = {(i, j): set(k for k in range(1, n+1)) for i in range(n) for j in
         #            range(n)}
         pos_vals = {}
@@ -130,10 +133,10 @@ class Skyscraper(object):
                 pos_vals[ij] = sort_list(vals)
         return pos_vals, num_vals
 
-    def update_possible_values(self, index, value, pos_vals):
+    def update_possible_values(self, index, value):
         i, j = index
         #print(pos_vals)
-        pos_vals_ = deepcopy(pos_vals)
+        pos_vals_ = deepcopy(self.pos_vals)
         for k in range(self.n):
             if value in pos_vals_[(i, k)]:
                 pos_vals_[(i, k)].remove(value)
@@ -144,30 +147,28 @@ class Skyscraper(object):
 
     def starting_board(self):
         board = [[0] * self.n for i in range(self.n)]
-        if self.obvious:
-            pos_vals, _ = self.possible_values()
-            for (i, j), v in pos_vals.items():
+        if self.use_obvious:
+            for (i, j), v in self.pos_vals.items():
                 if len(v) == 1:
                     board[i][j] = v[0]
         return board
 
-    # TODO: reconcile output types with smart_cell_order
-    def next_empty(self):
+    def naive_cell_order(self):
+        cell_order = []
         for i, row in enumerate(self.board):
             for j, val in enumerate(row):
                 if val == 0:
-                    return (i, j)
-        return False
+                    cell_order.append((i, j))
+        return cell_order
 
-    # TODO: reconcile output types with next_empty
     def smart_cell_order(self):
         if 0 not in [x for row in self.board for x in row]:
             return [], {}
         bins = defaultdict(list)
-        pos_vals, num_vals = self.possible_values()
-        line_order = [k for k, _ in sorted(num_vals.items(), key=lambda x: x[1])]
+        line_order = [k for k, _ in sorted(self.num_vals.items(),
+                                           key=lambda x: x[1])]
         already_counted = set()
-        order = []
+        cell_order = []
         for line in line_order:
             row_or_col, num = line.split('-')
             num = int(num)
@@ -175,24 +176,23 @@ class Skyscraper(object):
                 ind = 0
             else:
                 ind = 1
-            line_pos_vals = {k: v for k, v in pos_vals.items() if k[ind] == num}
+            line_pos_vals = {k: v for k, v in self.pos_vals.items() if k[ind] == num}
             line_ord = [k for k, _ in sorted(line_pos_vals.items(), key=lambda x: len(x[1]))
                         if k[ind] == num and k not in already_counted]
             already_counted |= set(line_ord)
-            order += line_ord
+            cell_order += line_ord
         # TODO: shouldn't have to do this
-        if self.obvious:
-            order = list(filter(lambda x: len(pos_vals[x]) > 1, order))
-        return order, pos_vals
+        if self.use_obvious:
+            cell_order = list(filter(lambda x: len(self.pos_vals[x]) > 1, cell_order))
+        return cell_order
 
     def smart_cell_order_old(self):
         bins = defaultdict(list)
-        pos_vals, _ = self.possible_values()
         #print(pos_vals)
-        for k, v in pos_vals.items():
+        for k, v in self.pos_vals.items():
             if self.board[k[0]][k[1]] == 0:
                 num_vals = len(v)
-                if self.obvious:
+                if self.use_obvious:
                     if num_vals > 1:
                         bins[num_vals].append(k)
                 else:
@@ -224,91 +224,53 @@ class Skyscraper(object):
             is_valid_col = self.is_clue_satisfied(self.column(cand_board, col), *clue_lt_rb)
 
         if is_valid_row and is_valid_col:
-            #if is_row_full:
-            #    print('Row: {}'.format(row))
-            #    print(cand_board)
-            #    print(clue_lt_rb)
-            #    print()
-            #elif is_col_full:
-            #    print('Col: {}'.format(col))
-            #    print(cand_board)
-            #    print(clue_lt_rb)
-            #    print()
-            self.board = cand_board
             return cand_board
         else:
             return None
 
-    def solve(self):
-        #print('hi')
-        coord = self.next_empty()
-        if not coord:
+    def solve(self):#, cell_order=[], value_options={}):
+        if self.cell_order == []:
             a = tuple(tuple(row) for row in self.board)
             return a
         else:
-            for cand in range(1, self.n+1):
+            #print(cell_order)
+            coord = self.cell_order[0]
+            for cand in self.pos_vals[coord]:
                 self.num_guesses += 1
                 old_board = self.board
+                old_cell_order = self.cell_order
+                old_pos_vals = self.pos_vals
                 cand_board = self.is_valid(cand, coord)
-                #print(cand, coord)
                 if cand_board is not None:
                     #print(cand_board)
                     self.board = cand_board
-                    next_board = self.solve()#board=cand_board)
+                    self.cell_order = self.cell_order[1:]
+                    if self.use_update_vals:
+                        self.pos_vals = self.update_possible_values(coord, cand)
+                    next_board = self.solve()
                     if next_board:
                         return next_board
                     else:
                         self.board = old_board
+                        self.cell_order = old_cell_order
+                        self.pos_vals = old_pos_vals
                         continue
                 else:
                     continue
             return False
 
-    def solve_smart(self, cell_order=[], value_options={}):
-        print('hi')
-        if cell_order == []:
-            # TODO: calls this when board is complete
-            # TODO: repeats obvious calculations
-            cell_order, value_options = self.smart_cell_order()
-            #print(cell_order)
-            #print(len(cell_order))
-            #print(board)
-        if cell_order == []:
-            a = tuple(tuple(row) for row in self.board)
-            return a
-        else:
-            #print(cell_order)
-            coord = cell_order[0]
-            for cand in value_options[coord]:
-                self.num_guesses += 1
-                old_board = self.board
-                cand_board = self.is_valid(cand, coord)
-                if cand_board is not None:
-                    #print(cand_board)
-                    self.board = cand_board
-                    value_options_ = self.update_possible_values(coord, cand, value_options)
-                    next_board = self.solve_smart(cell_order=cell_order[1:],
-                                                  value_options=value_options_)
-                    if next_board:
-                        return next_board
-                    else:
-                        self.board = old_board
-                        continue
-                else:
-                    continue
-            return False
-
-def solve_puzzle(clues, obvious=True):
-    skyscraper = Skyscraper(clues, obvious=obvious)
+def solve_puzzle(clues, **kwargs):#use_obvious=True, use_smart_cell_order=True,
+                 #use_update_vals=True):
+    skyscraper = Skyscraper(clues, **kwargs)# use_obvious=use_obvious, )
     solution = skyscraper.solve()
     num_guesses = skyscraper.num_guesses
     return solution, num_guesses
 
-def solve_puzzle_smart(clues, obvious=True):
-    skyscraper = Skyscraper(clues, obvious=obvious)
-    solution = skyscraper.solve_smart()
-    num_guesses = skyscraper.num_guesses
-    return solution, num_guesses
+#def solve_puzzle_smart(clues, use_obvious=True):
+#    skyscraper = Skyscraper(clues, use_obvious=use_obvious)
+#    solution = skyscraper.solve_smart()
+#    num_guesses = skyscraper.num_guesses
+#    return solution, num_guesses
 
 
 if __name__ == "__main__":
@@ -371,29 +333,20 @@ if __name__ == "__main__":
           ( 1, 5, 4, 3, 2, 6 ) )
     )
 
-    obvious = True
+    kwargs = {'use_obvious': True,
+              'use_smart_cell_order': True,
+              'use_update_vals': True}
 
-    #s,n = solve_puzzle(clues4[0], obvious=obvious)
+    #s,n = solve_puzzle(clues4[0], **kwargs)
     #correct = s == outcomes4[0]
-    #s,n = solve_puzzle(clues4[1], obvious=obvious)
+    #s,n = solve_puzzle(clues4[1], **kwargs)
     #correct = s == outcomes4[1]
-    #s,n = solve_puzzle(clues6[0], obvious=obvious)
-    #correct = s == outcomes6[0]
-    #s,n = solve_puzzle(clues6[1], obvious=obvious)
+    s,n = solve_puzzle(clues6[0], **kwargs)
+    correct = s == outcomes6[0]
+    #s,n = solve_puzzle(clues6[1], **kwargs)
     #correct = s == outcomes6[1]
-    #s,n = solve_puzzle(clues6[2], obvious=obvious)
+    #s,n = solve_puzzle(clues6[2], **kwargs)
     #correct = s == outcomes6[2]
-
-    #s,n = solve_puzzle_smart(clues4[0], obvious=obvious)
-    #correct = s == outcomes4[0]
-    #s,n = solve_puzzle_smart(clues4[1], obvious=obvious)
-    #correct = s == outcomes4[1]
-    #s,n = solve_puzzle_smart(clues6[0], obvious=obvious)
-    #correct = s == outcomes6[0]
-    #s,n = solve_puzzle_smart(clues6[1], obvious=obvious)
-    #correct = s == outcomes6[1]
-    s,n = solve_puzzle_smart(clues6[2], obvious=obvious)
-    correct = s == outcomes6[2]
 
     print(correct)
     print(n)
